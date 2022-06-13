6e85b8daab109b4c149c2aed77ee989fe9f07c47
magnusfreeborn <107408072+magnusfreeborn@users.noreply.github.com>
2022-06-13 08:13:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function pic() {
     var bgm = ['https://magnusfreeborn.000webhostapp.com/1.gif','https://magnusfreeborn.000webhostapp.com/1.gif', 'https://magnusfreeborn.000webhostapp.com/3.gif'];
 
     $('.random_bg').css({
-        'background-image' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ') repeat',
+        'background' : 'url('+ bgm[Math.floor(Math.random() * bgm.length)] + ') repeat',
         'background-size' : 'contain'
     });
 }
